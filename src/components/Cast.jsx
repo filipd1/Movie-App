0e7debf6354b8523a9a89c5c47ab0704df962b96
filipd1 <filip.dudziak2@o2.pl>
2025-06-21 11:02:43 +0200

    changed cast

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,43 +1,77 @@
 import { Link } from "react-router-dom"
 import "../css/Cast.css"
+import infoIcon from "../assets/info.svg"
 
 function Cast({ movie, credits }) {
 
     const directors = credits.crew?.filter(c => c.job === "Director")
 
     return (
-        <>
-            <p className="actor-title">{movie.title}'s cast:</p>
-            <div className="actor-slider">
-                {directors && directors.length > 0 && (
-                    directors.map((d, i) => (
-                        <div className="actor-card" key={i}>
-                            <Link to={`/person/${d.id}`}>
-                                <img src={`https://image.tmdb.org/t/p/w500${d.profile_path}`} alt={d.name} />
-                            </Link>
-                            <Link to={`/person/${d.id}`}>
-                                <p className="actor-name">{d.name}</p>
-                            </Link>
-                            <p className="actor-role">{d.job}</p>
-                        </div>
-                    ))
+        <div className="cast-container"> 
 
-                )}
-               
-                {credits.cast?.map((actor, i) => (
-                    actor.profile_path &&
-                    <div className="actor-card" key={i}>
-                        <Link to={`/person/${actor.id}`}>
-                            <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt={actor.name} />
-                        </Link>
-                        <Link to={`/person/${actor.id}`}>
-                            <p className="actor-name">{actor.name}</p>
-                        </Link>
-                        <p className="actor-role">{actor.character}</p>
-                    </div>
-                ))}    
+            <div className="movie-detailed-info">
+                <div className="flex">
+                    <img src={infoIcon} alt="movie-info" />
+                    <p className="movie-info-header"> Information</p>
+                </div>
+                <p>Director</p>
+                    {directors && directors.length > 0 && (
+                        directors.map((d, i) => (
+                            <Link to={`/person/${d.id}`} key={i}>
+                                <strong>{d.name} </strong>
+                            </Link>
+                        ))
+                    )}
+                <p>Original Language</p>
+                <strong>{movie.original_language.toUpperCase()}</strong>
+                <p>Production</p>
+                {movie.production_companies.slice(0, 1).map((c, i) => (
+                    <strong key={i}>{c.name}</strong>
+                ))}
+                <div className="flex mrg-top">
+                    <img src={infoIcon} alt="movie-info" />
+                    <p className="movie-info-header"> Money</p>
+                </div>
+                <p>Budget</p>
+                <strong>${movie.budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</strong>
+                <p>Revenue</p>
+                <strong>${movie.revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</strong>
             </div>
-        </>
+
+            <div className="cast-wrapper">
+                <p className="actor-title">{movie.title}'s cast:</p>
+                <div className="actor-slider">
+                    {directors && directors.length > 0 && (
+                        directors.map((d, i) => (
+                            <div className="actor-card" key={i}>
+                                <Link to={`/person/${d.id}`}>
+                                    <img src={`https://image.tmdb.org/t/p/w500${d.profile_path}`} alt={d.name} />
+                                </Link>
+                                <Link to={`/person/${d.id}`}>
+                                    <p className="actor-name">{d.name}</p>
+                                </Link>
+                                <p className="actor-role">{d.job}</p>
+                            </div>
+                        ))
+
+                    )}
+                
+                    {credits.cast?.map((actor, i) => (
+                        actor.profile_path &&
+                        <div className="actor-card" key={i}>
+                            <Link to={`/person/${actor.id}`}>
+                                <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt={actor.name} />
+                            </Link>
+                            <Link to={`/person/${actor.id}`}>
+                                <p className="actor-name">{actor.name}</p>
+                            </Link>
+                            <p className="actor-role">{actor.character}</p>
+                        </div>
+                    ))}    
+                </div>
+            </div>
+
+        </div>
     )
 }
 
