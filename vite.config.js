97eff01e6f8e291258f3eb0f298a84406ddc433c
filipd1 <filip.dudziak2@o2.pl>
2025-06-26 00:12:10 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: '/movie-search-engine/',
+  base: '/Movie-App/',
   plugins: [react(), svgr()],
 })
