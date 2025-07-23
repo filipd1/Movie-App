3b486e8a94f9ea9a33b20adb5b7636d64726ab98
filipd1 <filip.dudziak2@o2.pl>
2025-07-23 13:08:36 +0200

    token changes

--- server/index.cjs
+++ server/index.cjs
@@ -155,7 +155,7 @@ app.post("/api/login", async (req,res) => {
       const token = jwt.sign(
           { id: user._id, username: user.username },
           process.env.JWT_SECRET,
-          { expiresIn: "72h" }
+          { expiresIn: "30d" }
       )
 
       res.status(200).json({ message: "Login successful", token, user: { id: user._id, username: user.username} })
